9f099cfd1b020a1963bf23126eb618d99e51c9b8
HDBinath <bytecreator3@gmail.com>
2026-05-29 14:31:12 +0530

    add Meta Pixel code in head and body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,10 +49,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-K2299D48');`}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1600324478299871');
+fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body className={`${quicksand.variable} ${openSans.variable} ${poppins.variable} ${inter.variable} font-sans antialiased`}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2299D48"
 height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        <noscript><img height="1" width="1" style={{ display: 'none' }}
+src="https://www.facebook.com/tr?id=1600324478299871&ev=PageView&noscript=1" /></noscript>
         <div style={{ overflowX: 'clip', position: 'relative' }}>
           {children}
         </div>
